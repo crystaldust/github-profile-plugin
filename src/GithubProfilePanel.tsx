f5cb2022804a449922cbd2eff0dd207739ce6696
Zhen Ju <juzhen@huawei.com>
2022-03-14 18:21:10 +0800

    Code cleaning

--- src/GithubProfilePanel.tsx
+++ src/GithubProfilePanel.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PanelProps } from '@grafana/data';
-// interface Props extends PanelProps<SimpleOptions> {}
+import { getBackendSrv } from '@grafana/runtime';
+import { Button, Card } from '@grafana/ui';
+import './styles/_iconInformation.scss';
+import './styles/_cardInformation.scss';
+import './styles/GithubProfilePanel.scss';
+import './styles/custom.scss';
+import { IconCompany, IconLocation, IconTwitter, IconWebsite } from './Icons';
+
 
 export interface GithubProfilePanelOptions {
   dark: boolean;
-  // profile: object;
+  githubLoginVariableName: string;
 }
 
 interface GithubProfileProps<T = any> {
@@ -42,171 +49,209 @@ interface GithubProfileProps<T = any> {
   updated_at: string;
 }
 
-// interface GithubProfilePanelProps extends PanelProps<GithubProfilePanelOptions> {
-//   profile: GithubProfileProps;
-// }
+let joined = (profile: GithubProfileProps) => {
+  if (!profile.created_at) {
+    return 'Undefined';
+  }
+  const date = new Date(profile.created_at);
+  const d = date.toString().split(' ');
+  return `Joined ${d[2]} ${d[1]} ${d[3]}`;
+};
 
-// const URL = 'https://api.github.com';
-
-// function getGithubProfile(github_login) {
-//   return fetch(`${URL}/users/${github_login}`)
-//     .then((res) => res.json())
-//     .catch((error) => console.error(error));
-// }
-
-import './styles/_iconInformation.scss';
-import './styles/_cardInformation.scss';
-import './styles/GithubProfilePanel.scss';
-import { IconCompany, IconLocation, IconTwitter, IconWebsite } from './Icons';
-
-export const GithubProfilePanel: React.FC<PanelProps> = (props) => {
-  let state = {
-    unavailable: 'Not Available',
-  };
-
-  let github_id = props.data.series
-    .map((series) => series.fields.find((field) => field.name === 'github_login'))
-    .map((field) => field?.values.get(0));
-  console.log(github_id);
-  let profile: GithubProfileProps = {
-    login: 'crystaldust',
-    id: 2040659,
-    node_id: 'MDQ6VXNlcjIwNDA2NTk=',
-    avatar_url: 'https://avatars.githubusercontent.com/u/2040659?v=4',
-    gravatar_id: '',
-    url: 'https://api.github.com/users/crystaldust',
-    html_url: 'https://github.com/crystaldust',
-    followers_url: 'https://api.github.com/users/crystaldust/followers',
-    following_url: 'https://api.github.com/users/crystaldust/following{/other_user}',
-    gists_url: 'https://api.github.com/users/crystaldust/gists{/gist_id}',
-    starred_url: 'https://api.github.com/users/crystaldust/starred{/owner}{/repo}',
-    subscriptions_url: 'https://api.github.com/users/crystaldust/subscriptions',
-    organizations_url: 'https://api.github.com/users/crystaldust/orgs',
-    repos_url: 'https://api.github.com/users/crystaldust/repos',
-    events_url: 'https://api.github.com/users/crystaldust/events{/privacy}',
-    received_events_url: 'https://api.github.com/users/crystaldust/received_events',
-    type: 'User',
-    site_admin: false,
-    name: 'Zhen Ju',
-    company: 'Huawei',
-    blog: '',
-    location: 'Beijing, China',
-    email: 'juzhenatpku@gmail.com',
-    hireable: '',
-    bio: 'NodeJS/Golang/Python/now Rust Developer, he/him. Interested in all kinds of computer technologies.',
-    twitter_username: 'juzhenatpku',
-    public_repos: 145,
-    public_gists: 0,
-    followers: 32,
-    following: 23,
-    created_at: '2012-07-25T13:51:16Z',
-    updated_at: '2022-02-28T13:16:56Z',
-  };
-
-  let joined = () => {
-    if (!profile.created_at) {
-      return 'Undefined';
-    }
-    const date = new Date(profile.created_at);
-    const d = date.toString().split(' ');
-    return `Joined ${d[2]} ${d[1]} ${d[3]}`;
-  };
-
-  let name = () => {
-    if (profile.name) {
-      return String(profile.name).replace(/\b\w/g, (l) => l.toUpperCase());
-    } else {
-      return String(profile.login).replace(/\b\w/g, (l) => l.toUpperCase());
-    }
-  };
-
-  let company = () => {
-    const array = String(profile.company).split('');
-    if (array[0] === '@') {
-      array.splice(0, 1);
-    }
-    return array.join('');
-  };
+let name = (profile: GithubProfileProps) => {
+  if (profile.name) {
+    return String(profile.name).replace(/\b\w/g, (l) => l.toUpperCase());
+  } else {
+    return String(profile.login).replace(/\b\w/g, (l) => l.toUpperCase());
+  }
+};
+let company = (profile: GithubProfileProps) => {
+  const array = String(profile.company).split('');
+  if (array[0] === '@') {
+    array.splice(0, 1);
+  }
+  return array.join('');
+};
+let blogAddress = (profile: GithubProfileProps) => {
+  if (profile.blog === '' || profile.blog === undefined) {
+    return state.unavailable;
+  }
 
   return (
-    <div className="card dark">
-      <div className="information">
-        <img className="information__img" src={profile.avatar_url} alt="avatar" />
+    <a href={profile.blog} target="_blank" rel="noreferrer">
+      {profile.blog}
+    </a>
+  );
+};
 
-        <div className="information__container">
-          <h2 className="information__name">{name()}</h2>
-          <h3 className="information__user">
-            <a href={`https://github.com/${profile.login}`}>{`@${profile.login}`}</a>
-          </h3>
-          <p className="information__joined">{joined()}</p>
-        </div>
+let twitterAddress = (profile: GithubProfileProps) => {
+  if (!profile.twitter_username) {
+    return state.unavailable;
+  }
+  return (
+    <a href={`https://twitter.com/${profile.twitter_username}`} target="_blank" rel="noreferrer">
+      {profile.twitter_username}
+    </a>
+  );
+};
 
-        <p className={`information__description ${profile.bio ? '' : 'opasity'}`}>
-          {profile.bio ? profile.bio : 'This profile has no bio'}
-        </p>
+let companyAddress = (profile: GithubProfileProps) => {
+  if (!profile.company) {
+    return state.unavailable;
+  }
+  return (
+    <a href={`https://github.com/${company(profile)}`} target="_blank" rel="noreferrer">
+      {profile.company}
+    </a>
+  );
+};
 
-        <div className="information__statistic">
-          <div className="information__item">
-            <a href={`https://github.com/${profile.login}?tab=repositories`} target="_blank" rel="noreferrer">
-              <h4 className="information__item--title">Repos</h4>
-            </a>
-            <p className="information__item--content">{profile.public_repos ? profile.public_repos : '0'}</p>
-          </div>
+let state = {
+  unavailable: 'Not Available',
+};
 
-          <div className="information__item">
-            <a href={`https://github.com/${profile.login}?tab=followers`} target="_blank" rel="noreferrer">
-              <h4 className="information__item--title">Followers</h4>
-            </a>
-            <p className="information__item--content">{profile.followers ? profile.followers : '0'}</p>
-          </div>
-          <div className="information__item">
-            <a href={`https://github.com/${profile.login}?tab=followering`} target="_blank" rel="noreferrer">
-              <h4 className="information__item--title">Following</h4>
-            </a>
-            <p className="information__item--content">{profile.following ? profile.following : '0'}</p>
-          </div>
-        </div>
+let defaultProfile: GithubProfileProps = {
+  login: 'octocat',
+  id: 583231,
+  node_id: 'MDQ6VXNlcjU4MzIzMQ==',
+  avatar_url: 'https://avatars.githubusercontent.com/u/583231?v=4',
+  gravatar_id: '',
+  url: 'https://api.github.com/users/octocat',
+  html_url: 'https://github.com/octocat',
+  followers_url: 'https://api.github.com/users/octocat/followers',
+  following_url: 'https://api.github.com/users/octocat/following{/other_user}',
+  gists_url: 'https://api.github.com/users/octocat/gists{/gist_id}',
+  starred_url: 'https://api.github.com/users/octocat/starred{/owner}{/repo}',
+  subscriptions_url: 'https://api.github.com/users/octocat/subscriptions',
+  organizations_url: 'https://api.github.com/users/octocat/orgs',
+  repos_url: 'https://api.github.com/users/octocat/repos',
+  events_url: 'https://api.github.com/users/octocat/events{/privacy}',
+  received_events_url: 'https://api.github.com/users/octocat/received_events',
+  type: 'User',
+  site_admin: false,
+  name: 'The Octocat',
+  company: '@github',
+  blog: 'https://github.blog',
+  location: 'San Francisco',
+  email: '',
+  hireable: '',
+  bio: '',
+  twitter_username: '',
+  public_repos: 8,
+  public_gists: 8,
+  followers: 5207,
+  following: 9,
+  created_at: '2011-01-25T18:44:36Z',
+  updated_at: '2022-02-22T15:07:13Z',
+};
 
-        <div className="information__links">
-          <p className="information__link location">
-            <span className="information__icon">
-              <IconLocation />
-            </span>
-            {profile.location ? profile.location : state.unavailable}
-          </p>
+async function doGetGithbubProfile(login: String) {
+  return await getBackendSrv().datasourceRequest({
+    method: 'GET',
+    // the link shown below is made up & will not work but a legit http link is used for this
+    url: `https://api.github.com/users/${login}`,
+    headers: {
+      Authorization: 'token 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',
+      'Content-Type': 'application/json',
+    },
+  });
+}
 
-          <a href={`profile.blog`} className={`information__link ${profile.blog ? '' : 'opasity'}`}>
-            <span className="information__icon">
-              <IconWebsite />
-            </span>
-            {profile.blog === '' || profile.blog === undefined ? state.unavailable : profile.blog}
+type ProfileCache = { [k: string]: GithubProfileProps };
+let PROFILE_CACHE: ProfileCache = {};
+
+export const GithubProfilePanel: React.FC<PanelProps> = (props) => {
+  const [profile, setProfile] = useState(defaultProfile);
+  let varName = (props.options as GithubProfilePanelOptions).githubLoginVariableName;
+  if (!varName.startsWith('$')) {
+    varName = '$' + varName;
+  }
+  const currentGithubLogin = props.replaceVariables(varName);
+
+  if (currentGithubLogin) {
+    useEffect(() => {
+      if (PROFILE_CACHE.hasOwnProperty(currentGithubLogin)) {
+        console.debug('set profile from cache', currentGithubLogin)
+        setProfile(PROFILE_CACHE[currentGithubLogin]);
+      } else {
+        doGetGithbubProfile(currentGithubLogin)
+            .then((res: any) => {
+              setProfile(res.data);
+              PROFILE_CACHE[currentGithubLogin] = res.data;
+            })
+            .catch((e: any) => {});
+      }
+    }, [currentGithubLogin])
+  }
+
+  return (
+    <div>
+      <Card heading={name(profile)} className="headLineCard">
+        <Card.Figure>
+          <a href={`https://github.com/${profile.login}`}>
+            <img src={profile.avatar_url} alt="Avatar" className="cardAvatar" />
+          </a>
+        </Card.Figure>
+
+        <Card.Meta>
+          <a href={`https://github.com/${profile.login}`}>@{profile.login}</a>
+          <span>{joined(profile)}</span>
+        </Card.Meta>
+      </Card>
+
+      <Card className="headLineCard tailingLineCard">
+        <Card.Meta>{profile.bio}</Card.Meta>
+
+        <Card.Actions>
+          <a href={`https://github.com/${profile.login}?tab=repositories`} target="_blank" rel="noreferrer">
+            <Button fill="text" size="md">
+              {profile.public_repos ? profile.public_repos : '0'} Repos
+            </Button>
           </a>
 
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={`https://twitter.com/${profile.twitter_username}`}
-            className={`information__link ${profile.twitter_username ? '' : 'opasity'}`}
-          >
-            <span className="information__icon">
-              <IconTwitter />
-            </span>
-            {profile.twitter_username ? profile.twitter_username : state.unavailable}
+          <a href={`https://github.com/${profile.login}?tab=followers`} target="_blank" rel="noreferrer">
+            <Button fill="text" size="md">
+              {profile.followers ? profile.followers : '0'} Followers
+            </Button>
           </a>
 
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={`https://github.com/${company()}`}
-            className={`information__link ${profile.company ? '' : 'opasity'}`}
-          >
-            <span className="information__icon">
-              <IconCompany />
-            </span>
-            {profile.company ? profile.company : state.unavailable}
+          <a href={`https://github.com/${profile.login}?tab=followings`} target="_blank" rel="noreferrer">
+            <Button fill="text" size="md">
+              {profile.following ? profile.following : '0'} Followings
+            </Button>
           </a>
-        </div>
-      </div>
+        </Card.Actions>
+      </Card>
+
+      <Card className="headLineCard tailingLineCard">
+        <Card.Actions>
+          <span className="iconLink">
+            <IconLocation />
+            <span className="iconLinkText">{profile.location ? profile.location : state.unavailable}</span>
+          </span>
+
+          <span className="iconLink">
+            <IconWebsite />
+            <span className="iconLinkText">{blogAddress(profile)}</span>
+          </span>
+
+          {/*<a target="_blank" rel="noreferrer" href={`https://twitter.com/${profile.twitter_username}`}*/}
+          {/*    className={`${profile.twitter_username ? '' : 'opasity'}`}>*/}
+          <span className={`iconLink ${profile.twitter_username ? '' : 'opasity'}`}>
+            <IconTwitter />
+            <span className="iconLinkText">{twitterAddress(profile)}</span>
+          </span>
+          {/*</a>*/}
+
+          {/*<a target="_blank" rel="noreferrer" href={`https://github.com/${company(profile)}`}*/}
+          {/*    className={`${profile.company ? '' : 'opasity'}`}>*/}
+          <span className="iconLink">
+            <IconCompany />
+            <span className="iconLinkText">{companyAddress(profile)}</span>
+          </span>
+          {/*</a>*/}
+        </Card.Actions>
+      </Card>
     </div>
   );
 };
--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,18 @@
 import { PanelPlugin } from '@grafana/data';
-import { GithubProfilePanelOptions, AnotherGithubProfilePanel } from './AnotherGithubProfilePanel';
-// import { GithubProfilePanelOptions, GithubProfilePanel } from './GithubProfilePanel';
+import { GithubProfilePanelOptions, GithubProfilePanel } from './GithubProfilePanel';
 
-export const plugin = new PanelPlugin<GithubProfilePanelOptions>(AnotherGithubProfilePanel).setPanelOptions(
-  (builder) => {
-    return builder.addBooleanSwitch({
+export const plugin = new PanelPlugin<GithubProfilePanelOptions>(GithubProfilePanel).setPanelOptions((builder) => {
+  return builder
+    .addBooleanSwitch({
       path: 'dark',
       name: 'Panel theme',
       description: 'Theme of the panel(dark or light)',
       defaultValue: true,
-    }).addTextInput({
-        path: 'githubLoginVariableName',
-        name: 'GitHub login variable name',
-        description: 'The variable name to track current github login',
-        defaultValue: 'github_login'
+    })
+    .addTextInput({
+      path: 'githubLoginVariableName',
+      name: 'GitHub login variable name',
+      description: 'The variable name to track current github login',
+      defaultValue: 'github_login',
     });
-  }
-);
+});
